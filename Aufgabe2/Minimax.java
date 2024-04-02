465f514396a49fb3e2d3fec7f849fdaca347f502
FelixSubject211 <113304291+FelixSubject211@users.noreply.github.com>
2024-04-02 16:54:09 +0200

    alpha beta cutting

--- Aufgabe2/Minimax.java
+++ Aufgabe2/Minimax.java
@@ -2,15 +2,14 @@ package aufgaben.Aufgabe2;
 
 public class Minimax {
 
-    static int deep = 0;
-    static int maxDeep = 5;
-
     public static KalahBoard maxAction(KalahBoard board) {
         if (board.isFinished()) return null;
         int v = Integer.MIN_VALUE;
+        int alpha = Integer.MIN_VALUE;
+        int beta = Integer.MAX_VALUE;
         KalahBoard res = null;
         for (KalahBoard child: board.possibleActions()) {
-            int v1 = minValue(child);
+            int v1 = minValue(child, alpha, beta);
             if (v1 > v) {
                 v = v1;
                 res = child;
@@ -19,24 +18,24 @@ public class Minimax {
         return res;
     }
 
-    static int maxValue(KalahBoard board) {
-        if (deep >= maxDeep) return board.h1();
+    static int maxValue(KalahBoard board, int alpha, int beta) {
         if (board.isFinished()) return board.h1();
         int v = Integer.MIN_VALUE;
-        deep++;
         for (KalahBoard child: board.possibleActions()) {
-            v = Math.max(v, minValue(child));
+            v = Math.max(v, minValue(child, alpha, beta));
+            if (v >= beta) return v;
+            alpha = Math.max(alpha, v);
         }
         return v;
     }
 
-    static int minValue(KalahBoard board) {
-        if (deep >= maxDeep) return board.h1();
+    static int minValue(KalahBoard board, int alpha, int beta) {
         if (board.isFinished()) return board.h1();
         int v = Integer.MAX_VALUE;
-        deep++;
         for (KalahBoard child: board.possibleActions()) {
-            v = Math.min(v, maxValue(child));
+            v = Math.min(v, maxValue(child, alpha, beta));
+            if (v <= beta) return v;
+            beta = Math.min(beta, v);
         }
         return v;
     }
