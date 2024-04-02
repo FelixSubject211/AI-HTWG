1e343134018f70b3d8ef4a1bd0241438deae2e99
FelixSubject211 <113304291+FelixSubject211@users.noreply.github.com>
2024-04-02 16:42:30 +0200

    maxDeep

--- Aufgabe2/Minimax.java
+++ Aufgabe2/Minimax.java
@@ -2,6 +2,9 @@ package aufgaben.Aufgabe2;
 
 public class Minimax {
 
+    static int deep = 0;
+    static int maxDeep = 5;
+
     public static KalahBoard maxAction(KalahBoard board) {
         if (board.isFinished()) return null;
         int v = Integer.MIN_VALUE;
@@ -17,8 +20,10 @@ public class Minimax {
     }
 
     static int maxValue(KalahBoard board) {
+        if (deep >= maxDeep) return board.h1();
         if (board.isFinished()) return board.h1();
         int v = Integer.MIN_VALUE;
+        deep++;
         for (KalahBoard child: board.possibleActions()) {
             v = Math.max(v, minValue(child));
         }
@@ -26,8 +31,10 @@ public class Minimax {
     }
 
     static int minValue(KalahBoard board) {
+        if (deep >= maxDeep) return board.h1();
         if (board.isFinished()) return board.h1();
         int v = Integer.MAX_VALUE;
+        deep++;
         for (KalahBoard child: board.possibleActions()) {
             v = Math.min(v, maxValue(child));
         }
