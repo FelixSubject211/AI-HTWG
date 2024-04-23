2e000aace5f1e6b7af3fe261715ab7feb41fdd29
felixfischer <ff@mindhoox.de>
2024-04-23 15:19:15 +0200

    new overlab

--- Aufgabe3/packungsproblem.py
+++ Aufgabe3/packungsproblem.py
@@ -5,19 +5,19 @@ def packungsproblem(groesse_grosses_rechteck, kleine_rechtecke):
     problem = Problem()
 
     for i, (width, height) in enumerate(kleine_rechtecke):
-        print(i)
+        #print(i)
         positions = []
         x_range = range(0, groesse_grosses_rechteck[0] - width + 1)
         y_range = range(0, groesse_grosses_rechteck[1] - height + 1)
         if x_range.stop > 0 and y_range.stop > 0:
-            positions += [(x, y, "h", i, width, height) for x in x_range for y in y_range]
+            positions += [(x - 1, y - 1, "h", i, width, height) for x in x_range for y in y_range]
 
         x_range = range(0, groesse_grosses_rechteck[0] - height + 1)
         y_range = range(0, groesse_grosses_rechteck[1] - width + 1)
         if x_range.stop > 0 and y_range.stop > 0:
-            positions += [(x, y, "v", i, width, height) for x in x_range for y in y_range]
+            positions += [(x - 1, y - 1, "v", i, width, height) for x in x_range for y in y_range]
 
-        print(positions)
+        #print(positions)
         problem.addVariable(i, positions)
 
     for i1, klein1 in enumerate(kleine_rechtecke):
@@ -35,24 +35,28 @@ def overlab(k1, k2):
     (k1_x, k1_y, k1_a, k1_i, k1_w, k1_h) = k1
     (k2_x, k2_y, k2_a, k2_i, k2_w, k2_h) = k2
 
+    delta1x = k1_w if k1_a == "h" else k1_h
+    delta1y = k1_h if k1_a == "h" else k1_w
+
+    delta2x = k2_w if k2_a == "h" else k2_h
+    delta2y = k2_h if k2_a == "h" else k2_w
+
+    return rectangles_overlap(k1_x, k1_y, delta1x, delta1y, k2_x, k2_y, delta2x, delta2y)
 
 
-    if k1_a == "h" and k2_a == "h":
-        overlapH = k1_x + k1_w < k2_x or k2_x + k2_w < k1_x
-        overlapV = k1_y + k1_h < k2_y or k2_y + k2_h < k1_y
-        return overlapH and overlapV
-    if k1_a == "h" and k2_a == "v":
-        overlapH = k1_x + k1_w < k2_x or k2_x + k2_h < k1_x
-        overlapV = k1_y + k1_h < k2_y or k2_y + k2_w < k1_y
-        return overlapH and overlapV
-    if k1_a == "v" and k2_a == "h":
-        overlapH = k1_x + k1_h < k2_x or k2_x + k2_w < k1_x
-        overlapV = k1_y + k1_w < k2_y or k2_y + k2_h < k1_y
-        return overlapH and overlapV
-    if k1_a == "v" and k2_a == "v":
-        overlapH = k1_x + k1_h < k2_x or k2_x + k2_h < k1_x
-        overlapV = k1_y + k1_w < k2_y or k2_y + k2_w < k1_y
-        return overlapH and overlapV
+def rectangles_overlap(k1_x, k1_y, delta1_x, delta1_y, k2_x, k2_y, delta2_x, delta2_y):
+    k1_bottom_right_x = k1_x + delta1_x
+    k1_bottom_right_y = k1_y + delta1_y
+    k2_bottom_right_x = k2_x + delta2_x
+    k2_bottom_right_y = k2_y + delta2_y
+
+    if (k1_x < k2_bottom_right_x and
+            k1_bottom_right_x > k2_x and
+            k1_y < k2_bottom_right_y and
+            k1_bottom_right_y > k2_y):
+        return True
+    else:
+        return False
 
 
 groesse_grosses_rechteck = (7, 8)
